a388e8018f1fa6226a412ceb401bf998be15e943
AlexanderToles <alexandertoleseducation@gmail.com>
2025-04-01 18:47:54 -0500

    edited input and created print matrix function

--- problem1.cpp
+++ problem1.cpp
@@ -7,15 +7,46 @@ using namespace std;
 class Matrix {
     public:
     int size;
+    string name;
     std::vector<std::vector<int> > matrix;
 
     int getSize(){
         return size;
     }
+    int printMatrix(){
+        cout << name << ":\n";
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
     void setValue(int i, int j, int val){
         matrix[i][j] = val;    
     }
 };
+
+void readMatrix(vector<string> lines,int start,int end, Matrix &inMatrix){
+    int linePos = 0;
+    std::vector<std::vector<int> > tempMatrix;
+    for(int i = start;i < end; i++){
+        vector<int> row;
+        cout << lines[i] << "\n";
+        std::regex pattern("\\d+");  // Create a named regex variable
+        auto searchBegin = std::sregex_iterator(lines[i].begin(), lines[i].end(), pattern);
+        auto searchEnd = std::sregex_iterator();
+        for (sregex_iterator j = searchBegin; j != searchEnd; ++j)
+        {
+            smatch match = *j;
+            int match_int = stoi(match.str());
+            row.insert(row.begin(),match_int);
+        }
+        tempMatrix.insert(tempMatrix.begin(),row);
+    }
+    inMatrix.matrix = tempMatrix;
+
+}
 
 void scanFile(string fileName){
     fstream inputFile;
@@ -27,34 +58,36 @@ void scanFile(string fileName){
         string line; //current line from file
         smatch match; //matches from regex search
         int linePos = 0; //current line position
-
-        Matrix newMatrix; //create matrix
+        vector<string> lines;
+        Matrix matrixA, matrixB; //create matrix
+        matrixA.name = "Matrix A";
+        matrixB.name = "Matrix B";
 
         while(getline(inputFile,line)){
-            if(regex_search(line,match,regex ("^\\d+")) && linePos==0){ //only want the number located in the first line
+            lines.push_back(line);
+        }
+
+        for(int i = 0; i < lines.size(); i++){
+            if(regex_search(lines[i],match,regex ("^\\d+")) && i==0){ //only want the number located in the first line
 
                 int size = stoi(match[0]); //convert string to integer
                 cout << "size: " << size << "\n";
-                newMatrix.size = size;
-                linePos++;
+                matrixA.size = size;
+                matrixB.size = size;
             }
-            if (linePos > 0){
-                if (line != "\n"){
-                    if(linePos < linePos + newMatrix.size){
-                        std::regex pattern("\\d+");  // Create a named regex variable
-                        auto searchBegin = std::sregex_iterator(line.begin(), line.end(), pattern);
-                        auto searchEnd = std::sregex_iterator();
-                        for (sregex_iterator i = searchBegin; i != searchEnd; ++i)
-                        {
-                            smatch match = *i;
-                            string match_str = match.str();
-                            cout << match_str << '\n';
-                        }
+            else{
+                if(lines[i] != ""){
+                    int m2 = i+matrixA.size;
+                    readMatrix(lines,i,m2,matrixA);
+                    cout << "\n";
+                    readMatrix(lines,m2,m2+matrixB.size,matrixB);
+                    break;
+                } 
+            }
 
-                    }
-                }
-            }
         }
+        matrixA.printMatrix();
+        matrixB.printMatrix();
 
     }
     else {
