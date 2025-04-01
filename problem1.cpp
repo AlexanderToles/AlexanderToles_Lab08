3139e0e3b019b188b9e614d592a0c5aa919fba29
AlexanderToles <alexandertoleseducation@gmail.com>
2025-04-01 14:38:09 -0500

    added matrix class and updated scanfile to initialize matrix

--- problem1.cpp
+++ problem1.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 
 using namespace std;
+class Matrix {
+    public:
+    int size;
+    int getSize(){
+        return size;
+    }
+    void setValue(){
+
+    }
+};
+
 void scanFile(string fileName){
     fstream inputFile;
     inputFile.open(fileName, fstream::in);
     if(inputFile.is_open()){
-        cout << "File opened successfully!";
+        cout << "File opened successfully!\n";
+        string line;
+        smatch match;
+        int linePos = 0;
+        while(getline(inputFile,line)){
+            if(regex_search(line,match,regex ("^\\d+")) && linePos==0){
+
+                int size = stoi(match[0]);
+
+                cout << "size: " << size << "\n";
+                Matrix newMatrix;
+                newMatrix.size = size;
+                linePos++;
+            }
+        }
+
     }
     else {
-        cout << "Error: Failed to open file.";
+        cout << "Error: Failed to open file.\n";
     }
 }
 
