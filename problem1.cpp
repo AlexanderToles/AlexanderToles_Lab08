1ffabb49d8f7679754cc1176a98aa9a02ceae664
AlexanderToles <alexandertoleseducation@gmail.com>
2025-04-01 17:27:07 -0500

    updated function to search each line for matrix entries

--- problem1.cpp
+++ problem1.cpp
@@ -7,11 +7,13 @@ using namespace std;
 class Matrix {
     public:
     int size;
+    std::vector<std::vector<int> > matrix;
+
     int getSize(){
         return size;
     }
-    void setValue(){
-
+    void setValue(int i, int j, int val){
+        matrix[i][j] = val;    
     }
 };
 
@@ -19,19 +21,38 @@ void scanFile(string fileName){
     fstream inputFile;
     inputFile.open(fileName, fstream::in);
     if(inputFile.is_open()){
+
         cout << "File opened successfully!\n";
-        string line;
-        smatch match;
-        int linePos = 0;
+
+        string line; //current line from file
+        smatch match; //matches from regex search
+        int linePos = 0; //current line position
+
+        Matrix newMatrix; //create matrix
+
         while(getline(inputFile,line)){
-            if(regex_search(line,match,regex ("^\\d+")) && linePos==0){
+            if(regex_search(line,match,regex ("^\\d+")) && linePos==0){ //only want the number located in the first line
 
-                int size = stoi(match[0]);
-
+                int size = stoi(match[0]); //convert string to integer
                 cout << "size: " << size << "\n";
-                Matrix newMatrix;
                 newMatrix.size = size;
                 linePos++;
+            }
+            if (linePos > 0){
+                if (line != "\n"){
+                    if(linePos < linePos + newMatrix.size){
+                        std::regex pattern("\\d+");  // Create a named regex variable
+                        auto searchBegin = std::sregex_iterator(line.begin(), line.end(), pattern);
+                        auto searchEnd = std::sregex_iterator();
+                        for (sregex_iterator i = searchBegin; i != searchEnd; ++i)
+                        {
+                            smatch match = *i;
+                            string match_str = match.str();
+                            cout << match_str << '\n';
+                        }
+
+                    }
+                }
             }
         }
 
